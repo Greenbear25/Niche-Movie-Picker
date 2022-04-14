a1046b19ef3207b3527087b132eb4103f10cbcb8
Alex Blankenship <blankeyboat@gmail.com>
2022-04-14 10:15:48 -0500

    more java and some id tweaks on 2nd.html

--- script.js
+++ script.js
@@ -4,18 +4,15 @@ const button2 = document.getElementById("drama")
 const button3 = document.getElementById("horror")
 var buttonSelected = document.getElementsByClassName(".button")
 
-buttons.addEventListener('click', function(event){
-    // var genre = document.querySelectorAll('#comedy, #drama, #horror');
-    // console.log(genre)
-    
+buttons.addEventListener('click', function(event){    
     window.open(
         "2nd.html", "_blank"
     );
 })
 
 const onClick = function() {
-    localStorage.setItem('genre', this.id);
-    console.log(localStorage);
+    sessionStorage.setItem('genre', this.id);
+    console.log(sessionStorage);
   }
   document.getElementById("comedy").onclick = onClick;
   document.getElementById("drama").onclick = onClick;

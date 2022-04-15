3970c124e5dcd422932af3556dc4c83883ab6c7f
Cody Bae Claussen <c.claussen01@gmail.com>
2022-04-15 13:03:07 -0500

    Random

--- second_script.js
+++ second_script.js
@@ -40,15 +40,25 @@ var apiCall = function(){
       });
 }
 
+function random_item(items)
+{
+  
+return items[Math.floor(Math.random()*items.length)];
+     
+}
+
+var items = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49];
+console.log(random_item(items));
+
 var renderData = function(data){
-    var mTitle = data.results[0].title
-    var mTitle2 = data.results[1].title
-    var mTitle3 = data.results[2].title
-    var mTitle4 = data.results[3].title
-    var mPlot = data.results[0].plot
-    var mPlot2 = data.results[1].plot
-    var mPlot3 = data.results[2].plot
-    var mPlot4 = data.results[3].plot
+    var mTitle = data.results[random_item(items)].title
+    var mTitle2 = data.results[random_item(items)].title
+    var mTitle3 = data.results[random_item(items)].title
+    var mTitle4 = data.results[random_item(items)].title
+    var mPlot = data.results[random_item(items)].plot
+    var mPlot2 = data.results[random_item(items)].plot
+    var mPlot3 = data.results[random_item(items)].plot
+    var mPlot4 = data.results[random_item(items)].plot
     title.innerHTML = mTitle
     title2.innerHTML = mTitle2
     title3.innerHTML = mTitle3
@@ -57,7 +67,6 @@ var renderData = function(data){
     descript2.innerHTML = mPlot2
     descript3.innerHTML = mPlot3
     descript4.innerHTML = mPlot4
-
 }
 
 apiCall();
